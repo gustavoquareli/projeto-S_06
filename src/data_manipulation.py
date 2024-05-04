7a50c75555254f4e6e3f1116d9171d9e5452ce80
Gustavo Quareli <gustavo.quareli@gmail.com>
2024-05-04 18:16:12 -0300

    corrigindo algumas ordens, foi sincronizado com o site

--- src/data_manipulation.py
+++ src/data_manipulation.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-"""importando bibliotecas"""
 
+""""Essa linha de código serve para todas as funções que infelizmente não estão vindo de data_manipulation.py"""
 
 def columns_format(df):
     """Função para formatar os nomes das colunas"""
@@ -9,8 +9,19 @@ def columns_format(df):
     return df
 
 
-def columns_convert(df, column, type):
-    """Função para alterar o tipo das colunas"""
 
-    df[column] = df[column].astype(type)
+def columns_convert(df, columns, type):
+    """Função para alterar o tipo das colunas"""
+    for column in columns:
+        df[column] = df[column].astype(type)
+    return df
+
+def study_null_values(df, column):
+    """Função para estudar os valores nulos"""
+    return df[df[column].isnull()]
+
+
+def total_sales_columns(df, columns):
+    """Função para somar as vendas"""
+    df['total_sales'] = df[columns].sum(axis=1)
     return df
